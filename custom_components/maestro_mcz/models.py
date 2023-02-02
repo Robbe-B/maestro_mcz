d8e98adda2972f344d5e2e9c68f16482148cb47e
@crisicus <crisicus@gmail.com>
2023-02-02 17:53:15 +0100

    Add Model support

---
+++ custom_components/maestro_mcz/models.py
@@ -0,0 +1,47 @@
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorStateClass,
+)
+
+from homeassistant.helpers.entity import EntityCategory
+from homeassistant.const import TEMP_CELSIUS
+
+GENERIC_SENSORS = {
+    "state": ["Current State", None, "mdi:power", None, None, True, EntityCategory.DIAGNOSTIC],
+    "temp_amb_install": ["Temperature", TEMP_CELSIUS, "mdi:thermometer", SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, True, None],
+    "mode": ["Current Mode", None, "mdi:calendar-multiselect", None, None, True, EntityCategory.DIAGNOSTIC],
+    "temp_fumi": ["Exhaust Temperature", TEMP_CELSIUS, "mdi:thermometer", SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, True, EntityCategory.DIAGNOSTIC],
+    "temp_scheda": ["Board Temperature", TEMP_CELSIUS, "mdi:thermometer", SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, True, EntityCategory.DIAGNOSTIC],
+    "mode": ["Current Mode", None, "mdi:calendar-multiselect", None, None, True, EntityCategory.DIAGNOSTIC],
+    "vel_real_ventola_fumi": ["Exhaust Fan RPM", "rpm", "mdi:fan-chevron-up", None, SensorStateClass.MEASUREMENT, True, EntityCategory.DIAGNOSTIC],
+    "vel_real_coclea": ["Transport Screw RPM", "rpm", "mdi:screw-lag", None, None, SensorStateClass.MEASUREMENT, EntityCategory.DIAGNOSTIC],
+}
+
+GENERIC_FANS = {
+    "set_vent_v1": ["Fan 1", "mdi:fan", {"0", "1", "2", "3", "4", "5", "6"}, 1, "Fan", True, EntityCategory.CONFIG]
+}
+
+models = {
+    "CUTHDE08": {
+        "sensor": {
+            **GENERIC_SENSORS,
+            **{
+                # "temp_amb_install": ["Temperature", TEMP_CELSIUS, "mdi:thermometer", SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, True, None],
+            }
+        },
+        "fan": {
+            **GENERIC_FANS,
+            **{
+                # "set_vent_v1": ["Fan 1", ["0", "1", "2", "3", "4", "5", "6"], 1, "Fan"]
+            }
+        }
+    },
+    "SC12": {
+        "sensor": {
+            **GENERIC_SENSORS,
+        },
+        "fan": {
+            **GENERIC_FANS,
+        }
+    },
+}
--- custom_components/maestro_mcz/sensor.py
+++ custom_components/maestro_mcz/sensor.py
@@ -1,34 +1,46 @@
 """Platform for Sensor integration."""
-from . import MczCoordinator
+from . import MczCoordinator, models
 
 from homeassistant.components.sensor import (
-    SensorDeviceClass,
     SensorEntity,
-    SensorStateClass,
 )
 from homeassistant.core import callback
-from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
+ENTITY = "sensor"
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     stoveList = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(MczEntity(stove) for stove in stoveList)
+    entities = []
+    for stove in stoveList:
+        stove:MczCoordinator = stove
+        model = stove.maestroapi.State.nome_banca_dati_sel
+        for (prop, attrs) in models.models[model][ENTITY].items():
+            entities.append(MczEntity(stove, prop, attrs))
+
+    async_add_entities(entities)
 
 
 class MczEntity(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator):
+
+    _attr_has_entity_name = True
+
+    def __init__(self, coordinator, prop, attrs):
         super().__init__(coordinator)
+        [name, unit, icon, device_class, state_class, enabled_by_default, category] = attrs
         self.coordinator:MczCoordinator = coordinator
-        self._attr_name = "Temperature"
-        self._attr_native_unit_of_measurement = TEMP_CELSIUS
-        self._attr_device_class = SensorDeviceClass.TEMPERATURE
-        self._attr_state_class = SensorStateClass.MEASUREMENT
-        self._attr_unique_id = f"{self.coordinator._maestroapi.Status.sm_sn}_sensor"
-        self._attr_icon = "mdi:thermometer"
+        self._attr_name = name
+        self._attr_native_unit_of_measurement = unit
+        self._attr_device_class = device_class
+        self._attr_state_class = state_class
+        self._attr_unique_id = f"{self.coordinator._maestroapi.Status.sm_sn}-{prop}"
+        self._attr_icon = icon
+        self._prop = prop
+        self._enabled_default = enabled_by_default
+        self._category = category
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -38,8 +50,16 @@ class MczEntity(CoordinatorEntity, SensorEntity):
 
     @property
     def native_value(self):
-        return self.coordinator._maestroapi.State.temp_amb_install
+        return getattr(self.coordinator._maestroapi.State, self._prop)
 
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Return if the entity should be enabled when first added to the entity registry."""
+        return self._enabled_default
+
+    @property
+    def entity_category(self):
+        return self._category
     @callback
     def _handle_coordinator_update(self) -> None:
         self.async_write_ha_state()
