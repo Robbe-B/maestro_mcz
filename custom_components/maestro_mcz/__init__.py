bb6cc5e408030a6161da6ffe6733ce0418d65583
Robbe-B <103053873+Robbe-B@users.noreply.github.com>
2024-09-05 23:32:45 +0200

    Update __init__.py

--- custom_components/maestro_mcz/__init__.py
+++ custom_components/maestro_mcz/__init__.py
@@ -128,7 +128,7 @@ class MczCoordinator(DataUpdateCoordinator):
         sw_version = ""
 
         if self.maestroapi.Status.is_connected == True:
-            sw_version = f"{self._maestroapi.Status.sm_nome_app}.{self._maestroapi.Status.sm_vs_app}"
+            sw_version=f"{self._maestroapi.Status.sm_nome_app}.{self._maestroapi.Status.sm_vs_app}"
             + f", Panel:{self._maestroapi.Status.mc_vs_app}"
             + f", DB:{self._maestroapi.Status.nome_banca_dati_sel}"
         else:
