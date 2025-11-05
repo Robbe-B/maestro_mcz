70f7d519db239fd02119734e9b99d032b5586486
Robbe-B <103053873+Robbe-B@users.noreply.github.com>
2025-11-05 11:38:01 +0100

    Refactor options flow handler in config_flow.py
    Updated the options flow handler to remove dependency on config_entry.

--- custom_components/maestro_mcz/config_flow.py
+++ custom_components/maestro_mcz/config_flow.py
@@ -81,14 +81,10 @@ class MCZConfigFlow(ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
         """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
-    
+        return OptionsFlowHandler()
+        
 class OptionsFlowHandler(OptionsFlow):
-    """Handle a option flow for nut."""
-
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
+    """Handle a option flow for MCZ."""
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
